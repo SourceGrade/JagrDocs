39b5a70c0491ecd952d798d03b2a0f4cc29c0dfb
STG-Allen <ajacob715@gmail.com>
2021-03-19 19:44:12 -0400

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `JagrKt`,
+    defaultTitle: `JagrKt`,
+    siteTitleShort: `JagrKt`,
+    siteDescription: ``,
+    siteUrl: `http://www.jagrkt.org`,
+    siteAuthor: 'alexstaeding',
+    siteImage: ``,
+    siteLanguage: `en`,
+    themeColor: `#8257E6`,
+    basePath: `/`,
+  },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        githubUrl: `https://github.com/JagrKt/JagrKtWebsite`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocketseat Gatsby Themes`,
+        short_name: `RS Gatsby Themes`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: ``,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // trackingId: ``,
+      },
+    },
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://rocketdocs.netlify.com`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
